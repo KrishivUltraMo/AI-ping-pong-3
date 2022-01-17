1123ca1214b547faace53bed403fd480169bcc12
KrishivUltraMo <90205804+KrishivUltraMo@users.noreply.github.com>
2022-01-17 19:39:59 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,10 @@
+function setup(){
+   var canvas =  createCanvas(700,600);
+   canvas.parent('canvas');
+}
+
+   
+function preload() {
+   ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+   missed = loadSound("missed.wav");
+ }
